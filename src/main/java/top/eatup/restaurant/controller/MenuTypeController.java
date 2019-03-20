eb0e23dfa94bed08354a133340705e60cb8cec73
kei <keiwuhc@163.com>
2019-03-20 21:28:08 +0800

    generator生成代码覆盖原有代码

--- src/main/java/top/eatup/restaurant/controller/MenuTypeController.java
+++ src/main/java/top/eatup/restaurant/controller/MenuTypeController.java
@@ -3,9 +3,6 @@ package top.eatup.restaurant.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import top.eatup.restaurant.entity.MenuType;
-import top.eatup.restaurant.entity.MenuTypeExample;
-import top.eatup.restaurant.mapper.MenuTypeMapper;
 
 import java.util.List;
 
