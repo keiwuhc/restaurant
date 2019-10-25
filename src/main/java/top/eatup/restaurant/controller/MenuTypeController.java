af338cacb171daf67e610f721ffab52a9dd6f43d
kei <keiwuhc@163.com>
2019-10-25 11:19:26 +0800

    自动编译

--- src/main/java/top/eatup/restaurant/controller/MenuTypeController.java
+++ src/main/java/top/eatup/restaurant/controller/MenuTypeController.java
@@ -1,13 +1,17 @@
 package top.eatup.restaurant.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import top.eatup.restaurant.entity.MenuType;
 import top.eatup.restaurant.entity.MenuTypeExample;
 import top.eatup.restaurant.mapper.MenuTypeMapper;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 public class MenuTypeController {
@@ -22,6 +26,16 @@ public class MenuTypeController {
         MenuTypeExample mte = new MenuTypeExample();
         mte.createCriteria().andIdEqualTo("dsafdsafdsafdsafd");
         return menuTypeMapper.selectByExample(mte);
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public void save(@RequestBody MenuType menuType) {
+        UUID uuid = UUID.randomUUID();
+        String id = uuid.toString();
+        id = id.replaceAll("-", "");
+        System.out.println("ss在：" + id);
+        menuType.setId(id);
+        menuTypeMapper.insert(menuType);
     }
 
 }
